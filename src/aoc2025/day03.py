5e14a697b08ab64ad8c8d903c358fdd85563ad83
isak-lindbeck <isak.lindbeck@outlook.com>
2025-12-03 23:01:25 +0100

    Day03 cleanup

--- src/aoc2025/day03.py
+++ src/aoc2025/day03.py
@@ -9,23 +9,20 @@ def run(input_str: str) -> Tuple[int, int]:
     out_1, out_2 = 0, 0
 
     for line in input_str.splitlines():
-        first = max(line[:len(line) - 1])
-        idx = line.index(first)
-        second = max(line[idx + 1:])
-        i = int(f"{first}{second}")
-        out_1 += i
-
-        nums = ""
-        for i in range(0, 12):
-            remaining = 11 - i
-            length = len(line)
-            selection = line[:length - remaining]
-            m = max(selection)
-            line = line[line.index(m) + 1:]
-            nums += m
-        out_2 += int(nums)
+        out_1 += calculate(line, 2)
+        out_2 += calculate(line, 12)
 
     return out_1, out_2
+
+
+def calculate(line: str, num_digits: int) -> int:
+    result = ""
+    for i in range(0, num_digits):
+        remaining = num_digits - 1 - i
+        m = max(line[:len(line) - remaining])
+        line = line[line.index(m) + 1:]
+        result += m
+    return int(result)
 
 
 if __name__ == '__main__':
