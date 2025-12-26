9b495f4d8297f011e46b2473ecb1f486bd70d86f
isak-lindbeck <isak.lindbeck@outlook.com>
2025-12-26 03:59:39 +0100

    Day10 cleanup

--- src/aoc2025/day10.py
+++ src/aoc2025/day10.py
@@ -2,7 +2,6 @@ import sys
 from pathlib import Path
 
 import z3
-from z3 import Int
 
 from aoc2025.utils.io import check_output
 from aoc2025.utils.timed import Timed
@@ -12,28 +11,6 @@ sys.setrecursionlimit(1500)
 translation = str.maketrans("(){}.# ", "[][]01,", "[]")
 
 INF = sys.maxsize
-cache: dict[tuple[int, int], int] = {}
-
-def solve_1(desired_state: int, state_changes: list[int], current_state: int, change: int,
-            seen: list[int]) -> int | None:
-    key = (current_state, change)
-    if r := cache.get(key):
-        return r
-    next_state = current_state ^ change
-    if next_state == desired_state:
-        return 0
-    if next_state in seen:
-        cache[key] = INF
-        return cache[key]
-    seen = seen + [next_state]
-
-    result = INF
-    for state_change in state_changes:
-        if state_change == change:
-            continue
-        result = min(solve_1(desired_state, state_changes, next_state, state_change, seen), result)
-    cache[key] = result + 1
-    return cache[key]
 
 
 def run(input_str: str) -> tuple[int, int]:
@@ -44,7 +21,7 @@ def run(input_str: str) -> tuple[int, int]:
         section_2 = " ".join(split[1:-1])
         section_3 = split[-1]
 
-        desired_state: int = int(section_1.translate(translation)[::-1], 2)
+        current_state: int = int(section_1.translate(translation)[::-1], 2)
         buttons: tuple[list[int], ...] = eval(section_2.translate(translation))
         required_sums: list[int] = eval(section_3.translate(translation))
 
@@ -55,32 +32,44 @@ def run(input_str: str) -> tuple[int, int]:
                 bit_field |= (2 ** value)
             state_changes.append(bit_field)
 
-        global cache
-        cache = {}
-        out_1 += solve_1(desired_state, state_changes, 0, 0, [])
+        out_1 += solve_1(state_changes, current_state, [], {})
 
         optimizer = z3.Optimize()
         x_i = []
         for i in range(len(buttons)):
-            new_x = Int(f"x{i}")
-            x_i.append(new_x)
-            optimizer.add(new_x >= 0)
-
-        optimizer.minimize(sum(x_i))
+            x_i.append(z3.Int(f"x{i}"))
+            optimizer.add(x_i[-1] >= 0)
 
         for idx, required_sum in enumerate(required_sums):
-            xs_for_sum = [x_i[i] for i, button in enumerate(buttons) if idx in button]
-            expression = sum(xs_for_sum) == required_sum
+            xs_for_idx = [x_i[i] for i, button in enumerate(buttons) if idx in button]
+            expression = sum(xs_for_idx) == required_sum
             # print(expression)
             optimizer.add(expression)
         # print()
 
+        optimizer.minimize(sum(x_i))
         optimizer.check()
         model = optimizer.model()
-        res = sum([model[x].as_long() for x in model])
-        out_2 += res
+        out_2 += sum([model[x].as_long() for x in model])
 
     return out_1, out_2
+
+
+def solve_1(state_changes: list[int], current_state: int, seen: list[int], cache: dict[int, int]) -> int:
+    if current_state == 0:
+        return 0
+    if result := cache.get(current_state):
+        return result
+    if current_state in seen:
+        cache[current_state] = INF
+        return INF
+    seen = seen + [current_state]
+
+    result = INF
+    for state_change in state_changes:
+        result = min(solve_1(state_changes, current_state ^ state_change, seen, cache), result)
+    cache[current_state] = result + 1
+    return cache[current_state]
 
 
 if __name__ == '__main__':
